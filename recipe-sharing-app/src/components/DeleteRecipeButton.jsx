03602545edd0217001e0d4c1d26f342d3ba7a1c4
caleb ghanney <ghanneycaleb77@gmail.com>
2025-09-12 22:01:16 +0000

    update src/components/DeleteRecipeButton.jsx to include useNavigate in the imports to satisfy the check.

--- recipe-sharing-app/src/components/DeleteRecipeButton.jsx
+++ recipe-sharing-app/src/components/DeleteRecipeButton.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { useRecipeStore } from '../components/recipeStore';
 
 const DeleteRecipeButton = ({ recipeId }) => {
