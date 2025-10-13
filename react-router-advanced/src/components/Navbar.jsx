6b37c745ebac572c65c263a2cd6b8b260e26818f
caleb ghanney <ghanneycaleb77@gmail.com>
2025-10-13 04:01:12 +0000

    moved protectedroute file to components folder

--- react-router-advanced/src/components/Navbar.jsx
+++ react-router-advanced/src/components/Navbar.jsx
@@ -7,18 +7,23 @@ function NavBar() {
       <ul>
         <li>
           <Link to="/">Home</Link>
+          <NavLink to="/">Home</NavLink>
         </li>
         <li>
           <Link to="/about">About</Link>
+          <NavLink to="/about">About</NavLink>
         </li>
         <li>
           <Link to="/blog/1">Blog</Link>
+          <NavLink to="/blog/1">Blog</NavLink>
         </li>
         <li>
           <Link to="/profile">Profile</Link>
+          <NavLink to="/profile">Profile</NavLink>
         </li>
         <li>
           <Link to="/login">Login</Link>
+          <NavLink to="/login">Login</NavLink>
         </li>
       </ul>
     </nav>
