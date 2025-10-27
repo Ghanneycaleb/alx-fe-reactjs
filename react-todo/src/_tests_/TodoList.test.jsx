df97a22f595e0e75dadeebb3f963e135390d96e7
caleb ghanney <ghanneycaleb77@gmail.com>
2025-10-27 13:28:02 +0000

    test: Add vitest imports to fix undefined errors

--- react-todo/src/_tests_/TodoList.test.jsx
+++ react-todo/src/_tests_/TodoList.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen, fireEvent } from "@testing-library/react";
+import { describe, test, expect } from "vitest";
 import TodoList from "../components/TodoList";
 
 describe("TodoList Component", () => {
